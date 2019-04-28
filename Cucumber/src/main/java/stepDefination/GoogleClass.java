0aeeec27552fe9bc0e0b1bfe7865df4bd19b5955
Pranab Roy <Pranab Roy@Pranab.home>
2019-04-27 21:18:40 -0700

    Added author name on Google Stepdef

--- Cucumber/src/main/java/stepDefination/GoogleClass.java
+++ Cucumber/src/main/java/stepDefination/GoogleClass.java
@@ -16,6 +16,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+// Pranab Roy
 public class GoogleClass {
 WebDriver driver;
 /*	@Given("^User navigate to google$")
